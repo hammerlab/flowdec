64a248837daabbad3d7ef440f92a581e2e41c688
eric-czech <eric.allen.czech@gmail.com>
2019-01-06 07:22:55 -0500

    pypi classifiers

--- python/setup.py
+++ python/setup.py
@@ -26,8 +26,9 @@ if __name__ == '__main__':
             'Environment :: Console',
             'Operating System :: OS Independent',
             'Intended Audience :: Science/Research',
-            'Programming Language :: Python',
             'Topic :: Scientific/Engineering :: Bio-Informatics',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language:: Python:: 3.6'
         ],
         install_requires=requires,
         packages=['flowdec', 'flowdec.cmd', 'flowdec.nb'],
