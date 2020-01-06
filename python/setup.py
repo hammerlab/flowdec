f565324b09a1fbd0c8b676fe7a160f07a6d35d79
Eric Czech <eric@related.vc>
2020-01-06 06:29:13 -0500

    Package version bump

--- python/setup.py
+++ python/setup.py
@@ -23,7 +23,7 @@ except FileNotFoundError:
 if __name__ == '__main__':
     setup(
         name='flowdec',
-        version='1.0.7',
+        version='1.1.0',
         description="TensorFlow Implementations of Signal Deconvolution Algorithms",
         long_description=readme_markdown,
         long_description_content_type="text/markdown",
